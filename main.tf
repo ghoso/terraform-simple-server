d58c25292fdcbd483fe0baf543ec0be8f6595d24
Go Hosohara <go.hosohara@gmail.com>
2023-03-20 11:16:58 +0900

    run httpd on 8080

--- main.tf
+++ main.tf
@@ -5,7 +5,17 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-067871d950411e643"
   instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello World !" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  user_data_replace_on_change = true
+
   tags = {
     "Name" = "terraform-example"
   }
 }
+
+resource "aws_security
